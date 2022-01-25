f4eae028a82f8b41cb04bf09b1adf46b8a69cd7b
DvrDania <dvr.dania@gmail.com>
2022-01-25 16:56:32 +0330

    logo, img, fonts

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,39 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from "./components/HelloWorld.vue";
+import LoginForm from "./components/LoginForm.vue";
+import Logo from "./components/Logo.vue";
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <main>
+    <div class="relative flex items-center justify-center bg-gray-50">
+      <div class="absolute top-8 left-8 flex items-center">
+        <p class="mr-2 text-2xl font-normal text-violet-500">Vuexy</p>
+        <Logo class="text-violet-500" />
+      </div>
+      <img id="login-img" alt="login-img" src="./assets/images/login-img.svg" />
+    </div>
+    <LoginForm
+      class="flex flex-col items-center justify-center"
+      id="login-form"
+    />
+  </main>
 </template>
 
-<style>
+<style lang="postcss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  @apply font-roboto;
+}
+main {
+  min-height: 100vh;
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  grid-template-rows: 1fr;
+  grid-template-areas: "img form";
+}
+.login-img {
+  grid-area: img;
+}
+.login-form {
+  grid-area: form;
 }
 </style>
